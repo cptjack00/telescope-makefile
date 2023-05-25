e875704d0b24649ee4b3278e5976529e73a9e67b
quang <quangla2@vtx.viettel.vn>
2023-05-25 10:13:52 +0700

    Add compiledb to the extension

--- lua/telescope-makefile/config.lua
+++ lua/telescope-makefile/config.lua
@@ -5,6 +5,7 @@ config.defaults = {
 	makefile_priority = { ".", "build/" },
 	default_target = "[DEFAULT]", -- nil or string : Name of the default target | nil will disable the default_target
 	make_bin = "make", -- Custom makefile binary path, uses system make by def
+    compiledb_bin = "compiledb",
 }
 
 setmetatable(config, { __index = config.defaults })
